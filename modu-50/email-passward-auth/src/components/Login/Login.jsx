6ebe97c27d6f899f417183353625db5b5c07a773
shahidul shohid <shahidulislamshohid7gmail.com>
2024-11-10 11:24:03 +0600

    login form maked is done

--- modu-50/email-passward-auth/src/components/Login/Login.jsx
+++ modu-50/email-passward-auth/src/components/Login/Login.jsx
@@ -1,10 +1,92 @@
 
+import { NavLink } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase.init";
+import { useState } from "react";
+
 const Login = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">Login</h2>
+    
+    const [success, setSuccess] = useState(false)
+    const [loginError, setLoginError] = useState('')
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    
+   //reset status
+   setSuccess(false)
+   setLoginError('')
+  //login user
+  signInWithEmailAndPassword(auth, email, password)
+  .then(result => {
+    console.log(result.user)
+    setSuccess(true)
+  })
+  .catch(error => {
+    console.log(error.message)
+    setLoginError(error.message)
+  });
+
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+          {
+            success && <p className="text-green-600">User login successful</p>
+          }
+          {
+            loginError && <p className="text-red-600">{loginError}</p>
+          }
+          <p>New to this website? please <NavLink to='/signUp'>Sign Up</NavLink></p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- modu-50/email-passward-auth/src/components/SignUp/SignUp.jsx
+++ modu-50/email-passward-auth/src/components/SignUp/SignUp.jsx
@@ -1,69 +1,107 @@
-
-
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.init";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 const SignUp = () => {
-  const [error, setError] = useState('')
+  const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleSignUp = e => {
-        e.preventDefault()
-        
-        const email = e.target.email.value
-        const password = e.target.password.value
-        console.log(email, password)
+  const handleSignUp = (e) => {
+    e.preventDefault();
 
-        //reset error and status
-        setError('')
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const terms = e.target.terms.checked;
+    console.log(email, password, terms);
 
-        //create user with email and password
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-          console.log(result.user)
-        })
-        .catch(error => setError(error.message))
-
+    //reset error and status
+    setErrorMessage("");
+    setSuccess(false);
+    if(!terms){
+      setErrorMessage('Please accept our terms and condition')
+      return
     }
+
+    // validation password
+
+    if (password.length < 6) {
+      setErrorMessage("password should be 6 characters or longers");
+      return;
+    }
+
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    if (!passwordRegex.test(password)) {
+      setErrorMessage(
+        "at least one uppercase, one lowercase, one number, one special characters"
+      );
+      return;
+    }
+
+    //create user with email and password
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSuccess(true);
+      })
+      .catch((error) => setErrorMessage(error.message));
+    setSuccess(false);
+  };
   return (
-        <div className="card bg-base-100 w-full mx-auto my-6 max-w-sm shrink-0 shadow-2xl">
-            
+    <div className="card bg-base-100 w-full mx-auto my-6 max-w-sm shrink-0 shadow-2xl">
       <h1 className="text-3xl font-bold text-center">Sign Up Now</h1>
-          <form onSubmit={handleSignUp} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email" name="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password" name="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign Up</button>
-            </div>
-            {
-              error && <p className="text-red-600">{error}</p>
-            }
-          </form>
+      <form onSubmit={handleSignUp} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            className="input input-bordered"
+            required
+          />
         </div>
+        <div className="form-control relative">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="password"
+            className="input input-bordered"
+            required
+          />
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="btn btn-xs absolute right-2 top-12"
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+          <div className="form-control">
+            <label className="label justify-start gap-3 cursor-pointer">
+              <input type="checkbox" name="terms" className="checkbox" />
+              <span className="label-text">Accept Our Terms and Conditions</span>
+            </label>
+          </div>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Sign Up</button>
+        </div>
+        {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+        {success && <p className="text-green-600">Sign up is successful</p>}
+        <p>Already have an account Please?<Link to='/login'>Login</Link></p>
+      </form>
+    </div>
   );
 };
 
